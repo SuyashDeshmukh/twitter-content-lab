b041ff7b2b85f8d993cdf8e3b345090c540dfdb2
SuyashDeshmukh <suyash.deshmukh22@gmail.com>
2018-06-08 10:18:05 -0700

    Test Commit 4

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname +'/../dist'));
-app.get('/' ,express.static(__dirname + '/../dist'));
+
+app.use(express.static(path.join(__dirname +'/../dist')));
+app.get('/' ,function(req,res) {
+    res.send(200);
+});
 app.listen(process.env.PORT || 8080);
 
 
