f54a468b2eb23a10f1c4ca9f7454f04d0bc4aabb
SuyashDeshmukh <suyash.deshmukh22@gmail.com>
2018-06-08 10:35:39 -0700

    Test Commit 8

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname +'/../dist/heroku-test-app')));
 
 app.get('/' ,function(req,res) {
-    res.sendFile(express.static(path.join(__dirname + '/../dist/heroku-test-app/index.html')));
+    res.sendFile('index.html');
 });
 app.listen(process.env.PORT || 8080);
 
